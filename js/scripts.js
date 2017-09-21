3472b241d645a9f5fb9db18fd6fd4894c7a5f269
VeronicaMelnikova <31203184+VeronicaMelnikova@users.noreply.github.com>
2017-09-21 15:00:29 +0400

    мелкие правки

--- js/scripts.js
+++ js/scripts.js
@@ -27,20 +27,20 @@ close.addEventListener("click", function (evt) {
 
 form.addEventListener("submit", function (evt) {
   if (!login.value || !email.value) {
-      evt.preventDefault();
-      popup.classList.remove("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
-} else {
-      localStorage.setItem("login", login.value);
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    localStorage.setItem("login", login.value);
     }
 });
 
 window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
-        popup.classList.remove("modal-error");
-      }
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
+  }
 });
